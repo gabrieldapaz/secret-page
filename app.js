ad0f0dba62dfe4fd5735415242e97699278a9bd9
gabrieldapaz <mgpp@cin.ufpe.br>
2020-05-14 19:22:59 -0300

    setup passaport, sessions and creating an user model

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-var express = require("express");
+var express               = require("express");
+    mongoose              = require("mongoose");
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://localhost/auth_demo_app", {useUnifiedTopology:true, useNewUrlParser:true});
 
 var app = express();
 app.set("view engine", "ejs");
+
+app.use(require("express-session")({
+    secret: "Dale que dale bença",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
